e71e7c4a7c1154accda6db57cff754572d0d35ee
Aya6909 <aayaasimkul255@gmail.com>
2025-04-21 12:31:51 +0500

    ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,29 +1,26 @@
 import {useCart} from "./CartContext";
 import './App.css'
 
-
 const products =[
-  {id: 1, name: "Book", price: 2000},
-  {id: 2, name: "Pen", price: 500},
-  {id: 3, name: "Notebook", price: 1500},
-  {id: 4, name: "Backpack", price: 5000},
+  {id: 1,name:"Aple", price:1000},
+  {id: 2,name:"Pen", price:300},
+  {id: 3,name:"bag", price: 1700},
+  {id: 4,name:"banan", price:2300},
 ];
 
 export function ProductList(){
-  const{ dispatch } = useCart();
-  return(
-    <div>
+  const{dispatch} =useCart();
+  return(<div>
       <h2>Products</h2>
       {products.map((item) =>(
         <div key={item.id} style={{ marginBottom: "10px" }}>
-          <span>{item.name} - {item.price} ₸</span>
+          <span>{item.name} - {item.price}₸</span>
           <button
-            onClick={() => dispatch({ type: "ADD_ITEM", payload: item })}
+            onClick={() => dispatch({type: "ADD_ITEM", payload: item})}
             style={{ marginLeft:"10px" }}>add to Car</button>
         </div>))}
     </div>
-  );
-}
+  );}
 
 
 
